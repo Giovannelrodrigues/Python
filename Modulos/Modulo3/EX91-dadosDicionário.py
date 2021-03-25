a58396a79cf2b286d50dfca38b8761c27aa18b73
Giovanne Lisboa Rodrigues <79953718+Giovannelrodrigues@users.noreply.github.com>
2021-03-25 18:20:17 -0300

    Update EX91-dadosDicionário.py

--- Modulos/Modulo3/EX91-dadosDicionário.py
+++ Modulos/Modulo3/EX91-dadosDicionário.py
@@ -11,9 +11,10 @@ for contador in range(1,5):
 
 rank = sorted(Numeros.items(), key=itemgetter(1), reverse=True)
 print('='* 30)
-print('{"O ranking dos Jogadores"}')
+print("O ranking dos Jogadores")
 for ind, valor in enumerate(rank):
     print(f'{ind + 1}ª lugar = {valor[0]} tirou {valor[1]}')
+    sleep(1)
 print('='* 30)
    
     
